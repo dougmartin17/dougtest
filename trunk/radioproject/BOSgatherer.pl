025ab3b41ffd8f2878f9b40e8dbbdceb93e9fb6b
doug.martin <doug.martin@04f5fd3a-1d11-11df-8da0-9131547f57cf>
2010-05-17 22:17:36 +0000

    first pass at parsing the webpage. Need to now add to database.

---
+++ trunk/radioproject/BOSgatherer.pl
@@ -0,0 +1,48 @@
+use strict;
+use HTML::TreeBuilder;
+use LWP::Simple;
+
+my $TITLECLASS = "sTWBOS";
+my $LASTPLAYEDCLASS = "sDtWBOS";
+my $BANDCLASS = "sAWBOS";
+
+my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
+my $timestamp = sprintf "%02d/%02d/%4d %02d:%02d:%02d\n", $mon+1,$mday,$year+1900,$hour,$min,$sec;
+
+my $rURL = "http://www.wbos.biz/lastplayed.aspx";
+print "Start time: $timestamp\n";
+
+my $rTree = HTML::TreeBuilder->new;
+
+
+#get the html and create the tree
+
+my $rContent = get($rURL);
+$rTree->parse($rContent);
+
+my @rRows = $rTree->find('td');
+foreach my $r (@rRows)
+{
+   
+   my @t = $r->look_down('class' => $BANDCLASS);
+   my $band = $t[0] ? $t[0]->as_trimmed_text : '';
+      
+   my @t = $r->look_down('class' => qr/$LASTPLAYEDCLASS/);
+   my $lp = $t[0] ? $t[0]->as_trimmed_text : '';
+   
+   
+   my @t = $r->look_down('class' => qr/$TITLECLASS/);
+   my $title = $t[0] ? $t[0]->as_trimmed_text : '';
+   
+   
+   if($lp & $band & $title)
+   {
+      print "Band: [$band]\n";
+      print "Last played: [$lp]\n";
+      print "Title: [$title]\n";
+   }
+} # end foreach my $r (@rRows)
+   
+   
+   
+#$we = $wTree->look_down(sub { $_[0]->tag() eq 'meta' and $_[0]->attr('name') =~ /Description/i }
