6f051bce96b9c4deb483443ec0f1ac43e842ade0
doug.martin <doug.martin@04f5fd3a-1d11-11df-8da0-9131547f57cf>
2010-05-24 20:36:45 +0000

    Broke up DB commands added in timestamp fixer function.
    Should be ready for add to DB

--- trunk/radioproject/BOSgatherer.pl
+++ trunk/radioproject/BOSgatherer.pl
@@ -2,6 +2,9 @@ use strict;
 use HTML::TreeBuilder;
 use LWP::Simple;
 
+# PERL MODULE for DB access
+use DBI;
+
 my $TITLECLASS = "sTWBOS";
 my $LASTPLAYEDCLASS = "sDtWBOS";
 my $BANDCLASS = "sAWBOS";
@@ -21,6 +24,7 @@ my $rContent = get($rURL);
 $rTree->parse($rContent);
 
 my @rRows = $rTree->find('td');
+
 foreach my $r (@rRows)
 {
    
@@ -39,7 +43,10 @@ foreach my $r (@rRows)
    {
       print "Band: [$band]\n";
       print "Last played: [$lp]\n";
+      my $newlp = convertLastPlayed($lp);
+      print "converted lastplayed time: $newlp\n";
       print "Title: [$title]\n";
+      print "\n";
    }
 } # end foreach my $r (@rRows)
    
@@ -47,43 +54,88 @@ foreach my $r (@rRows)
    
 #$we = $wTree->look_down(sub { $_[0]->tag() eq 'meta' and $_[0]->attr('name') =~ /Description/i }
 
-
+####let's try DB
+my $db = openDBconnection();
+runDBcommand($db);
+closeDBconnection($db);
+###end of db stuff
 
 ####DATABASE connect/stuff
-
-# PERL MODULE
-use DBI;
-
-# CONFIG VARIABLES
-my $host = "192.168.1.69";
-my $port = 3306;
-my $database = "playlistproject";
-my $tablename = "playlists";
-#i've created this user in the db, not sure on permissions.
-my $user = "dbuser";
-my $pw = "dbuser_pw";
-$user='root';
-$pw='black';
-
-# PERL MYSQL CONNECT
-#$connect = Mysql->connect($host, $database, $user, $pw);
-my $dbh = DBI->connect("dbi:mysql:database=$database; host=$host;", $user,$pw)
-    or die "Connecting from Perl to MySQL database failed: $DBI::errstr";
-# Prepare the SQL statement
-my $sth= $dbh->prepare("SELECT * from t_playlists;")
-  or die $DBI::errstr;
-
-# Send the statement to the server
-$sth->execute();
-
-my $numRows = $sth->rows;
-print "Rows returned: $numRows\n";
-
-my @row;
-while ( @row = $sth->fetchrow_array )
+sub openDBconnection
 {
-  print "@row\n";
+   # CONFIG VARIABLES
+   my $host = "192.168.1.69";
+         
+   my $port = 3306;
+   my $database = "playlistproject";
+   my $tablename = "playlists";
+   #i've created this user in the db, not sure on permissions.
+   my $user = "dbuser";
+   my $pw = "dbuser_pw";
+   $user='root';
+   $pw='black';
+
+   # PERL MYSQL CONNECT
+   #$connect = Mysql->connect($host, $database, $user, $pw);
+   my $dbh = DBI->connect("dbi:mysql:$database:$host:$port;", $user,$pw)
+    or die "Connecting from Perl to MySQL database failed: $DBI::errstr";
+    
+    return $dbh;
 }
 
-# Close the connection
-$dbh->disconnect or die $DBI::errstr;
+sub closeDBconnection
+{
+   my $dbh = shift(@_);
+   
+   # Close the connection
+   $dbh->disconnect or die $DBI::errstr;
+}
+
+sub runDBcommand
+{
+   my $dbh = shift(@_);
+      # Prepare the SQL statement
+      my $sth= $dbh->prepare("SELECT * from t_playlists;")
+      or return $DBI::errstr;
+   
+      # Send the statement to the server
+      $sth->execute();
+   
+      my $numRows = $sth->rows;
+      print "Rows returned: $numRows\n";
+   
+      my @row;
+      while ( @row = $sth->fetchrow_array )
+      {
+      print "@row\n";
+      }
+}
+
+
+
+#convert the last played time we get from BOS to the mysql DATETIME format of 1900-01-01 00:00:00
+sub convertLastPlayed
+{
+   my $timestamp = shift(@_);
+
+   $timestamp =~ /(\d+)\/(\d+)\/(\d+) @ (\d+):(\d+) (\w{2})/;
+   my $month = $1;
+   my $day = $2;
+   my $year = $3;
+   
+   my $hour = $4;
+   my $minutes = $5;
+   my $ampm = $6;
+   
+   if($ampm eq 'pm')
+   {
+      #convert to 24 hour clock
+      $hour = $hour + 12;
+   }
+   
+   
+   my $newdate = sprintf("%4d-%02d-%02d %02d:%02d:00", $year,$month,$day,$hour,$minutes);
+   
+   
+   return $newdate;
+}
