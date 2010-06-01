682d1a07bcfc6771ab498a3ca55ece503d08c993
doug.martin@gmail.com <doug.martin@gmail.com@04f5fd3a-1d11-11df-8da0-9131547f57cf>
2010-06-01 02:49:58 +0000

    Updated to what should be a working FNXgatherer.pl Also had to add a nullcheck on band name because they throw a placement for ads in the XML. This code should probably be run >30 minutes and maybe 15 minutes to be safe.

--- radioproject/FNXgatherer.pl
+++ radioproject/FNXgatherer.pl
@@ -5,13 +5,8 @@ use LWP::Simple;
 #for parsing the XML we retrieve from FNX
 use XML::Simple;
 
-
 # PERL MODULE for DB access
 use DBI;
-
-my $TITLECLASS = "sTWBOS";
-my $LASTPLAYEDCLASS = "sDtWBOS";
-my $BANDCLASS = "sAWBOS";
 
 my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
 my $timestamp = sprintf "%02d/%02d/%4d %02d:%02d:%02d\n", $mon+1,$mday,$year+1900,$hour,$min,$sec;
@@ -32,6 +27,9 @@ my $xml = new XML::Simple;
 # read XML file
 my $data = $xml->XMLin($rContent);
 
+#open DB connection
+my $db = openDBconnection();
+
 #iterate through the elements in XML.
 foreach my $e (@{$data->{'nowplaying-info'}})
 {
@@ -39,13 +37,76 @@ foreach my $e (@{$data->{'nowplaying-info'}})
    #get individual parts out of the timestamp
    ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime($e->{timestamp});
    #reformat the time to mysql datetime
-   my $newtime = sprintf("%4d-%02d-%02d %02d:%02d:%02d", $year + 1900,$mon,$mday,$hour,$min,$sec);
-   
+   my $newtime = sprintf("%4d-%02d-%02d %02d:%02d:%02d", $year + 1900,$mon+1,$mday,$hour,$min,$sec);
+
+   my $band = $e->{property}->{track_artist_name}->{content};
+   my $title = $e->{property}->{cue_title}->{content};
+
    print "time: " . $newtime, "\n";
-   print "mount: " .$e->{mountName}, "\n";
-   print "band name: " .$e->{property}->{track_artist_name}->{content}, "\n";
-   print "song title: " .$e->{property}->{cue_title}->{content}, "\n";
-   print "\n"
+   print "band name: " .$band, "\n";
+   print "song title: " .$title, "\n";
+   print "\n";
+
+
+   $band =~ s/'/./g;
+   $title =~ s/'/./g;
+   
+#apparently there can be ads so we need a null check on band name
+   if($band)
+   {
+      dbInsertData($db, $band, $title, $newtime);
+   }
+
 }
 
-print @{$data->{'nowplaying-info'}}." songs\n";
+#close the db connection
+closeDBconnection($db);
+
+print "Finished running\n";
+
+
+####DATABASE connect/stuff
+sub dbInsertData
+{
+   my ($dbh, $b, $t, $lp) = @_;
+
+      # Prepare the SQL statement
+my $cmd = "insert into t_playlists (bandname,songtitle,timestamp,station)
+			values('$b','$t','$lp','wfnx');";
+#print "SQL command: " . $cmd."\n";
+      my $sth= $dbh->prepare($cmd)
+      or return $DBI::errstr;
+   
+      # Send the statement to the server
+      $sth->execute() or return $DBI::errstr;
+
+}
+sub openDBconnection
+{
+   # CONFIG VARIABLES
+   my $host = "192.168.1.69";
+         
+   my $port = 3306;
+   my $database = "playlistproject";
+   my $tablename = "playlists";
+   #i've created this user in the db, not sure on permissions.
+   my $user = "dbuser";
+   my $pw = "dbuser_pw";
+   $user='root';
+   $pw='black';
+
+   # PERL MYSQL CONNECT
+   #$connect = Mysql->connect($host, $database, $user, $pw);
+   my $dbh = DBI->connect("dbi:mysql:$database:host=:$host:port=$port;", $user,$pw)
+    or die "Connecting from Perl to MySQL database failed: $DBI::errstr";
+    
+    return $dbh;
+}
+
+sub closeDBconnection
+{
+   my $dbh = shift(@_);
+   
+   # Close the connection
+   $dbh->disconnect or die $DBI::errstr;
+}
